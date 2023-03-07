d9b4864b618641751b7ff980122e03a8c9f128d9
KrisRCBezerra <kris123.rosa@gmail.com>
2023-03-07 15:14:26 -0300

    Desenvolvimento de classe Nota para funcionamento do código

--- logica.py
+++ logica.py
@@ -1,6 +1,26 @@
 import datetime
-produto = input("Digite o nome do produto: ")
-data_str = input("Digite a data de emissão (YYYY-MM-DD): ")
+class Nota:
+    def __init__(self, produto, data, status):
+        self.produto = produto
+        self.data = data
+        self.status = "em aberto"
+        
+    def adicionar_produto(self, produto):
+        self.produto = input("Digite o nome do produto: ")
+        print(f'Produto: {self.produto}, adicionado com sucesso')
 
-ano, mes, dia = map(int, data_str.split("-"))
-data = datetime.date(ano, mes, dia)
+    def adicionar_data(self, data):
+        self.data = input("Digite a data de emissão (YYYY-MM-DD): ")
+        ano, mes, dia = map(int, data.split("-"))
+        self.data = data = datetime.date(ano, mes, dia)
+        print(f'Data de emissão atualizada para {data}')
+
+    def mudar_status(self, status):
+        self.status = input("Digite o status da nota (cancelado, ok): ")
+        if self.status == "ok":
+            self.status = "ok"
+        elif self.status == "cancelado":
+            self.status = "cancelado"
+        else:
+            self.status = "em aberto"
+        print(f'Status atualizado para {self.status}')
