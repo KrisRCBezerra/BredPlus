385e1fa4997e84c921711f8de15fb2d5db1bb1d6
KrisRCBezerra <kris123.rosa@gmail.com>
2023-03-07 13:41:30 -0300

    Atualização de código Adição dos inputs

--- logica.py
+++ logica.py
@@ -1,1 +1,6 @@
-# teste
+import datetime
+produto = input("Digite o nome do produto: ")
+data_str = input("Digite a data de emissão (YYYY-MM-DD): ")
+
+ano, mes, dia = map(int, data_str.split("-"))
+data = datetime.date(ano, mes, dia)
